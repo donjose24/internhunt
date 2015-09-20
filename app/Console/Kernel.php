503431dbc10f8121c63ac8b923e63889ca7d0a78
jmramos02 <ramosjosemari@gmail.com>
2015-09-20 11:27:04 +0800

    delete unnecessary middleware

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     ];
 
     /**
-     * Define the application's command schedule.
+     * Define the application's command schedule
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
